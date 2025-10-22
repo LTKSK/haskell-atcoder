8614171263d34e32673bd7d0bcab6228a2b6e7a8
LTKSK <ltksk0714@gmail.com>
2025-10-23 08:20:19 +0900

    tessoku_cb

---
+++ tessoku_book_cb/main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE LambdaCase #-}
+import Data.Char (digitToInt, isSpace)
+import Data.List (sort, isSuffixOf, isPrefixOf, unfoldr, tails)
+import Data.Set(fromList, toList)
+import Control.Monad (replicateM, msum, forM_)
+import Control.Monad.RWS (MonadState(put))
+import qualified Data.ByteString.Char8 as BS
+
+import qualified Data.Set as S
+import Data.Ix
+import Data.Array.IArray
+import Data.Array(Array)
+import Data.Array.Unboxed(UArray)
+
+ints :: IO [Int]
+ints = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+intwo =
+    ints >>= \case
+        [x, y] -> return (x,y)
+        _ -> error "引数足りない"
+
+getMatInt :: Int -> Int -> IO (UArray (Int, Int) Int)
+-- concatで多次元配列を1次元配列に
+getMatInt h w = listArray ((0,0), (h-1, w-1)) . concat <$> replicateM h ints
+
+binSearch :: (Int -> Bool) -> Int -> Int -> Int
+binSearch f ok ng
+  | abs (ok - ng) <= 1 = ok
+  | otherwise =
+    let mid = (ok + ng) `div` 2
+    in if f mid
+        then binSearch f mid ng -- 条件を満たすならmidをokに
+        else binSearch f ok mid -- 逆はngをmidに
+
+main :: IO ()
+main = do
+    [n] <- ints
+    as <- ints
+    let triples = [()| (x:ys) <- tails as, (y:zs) <- tails ys, z <- zs, (x+y+z) == 1000]
+
+    putStrLn $ if null triples then "No" else "Yes"
