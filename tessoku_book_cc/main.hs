d01a1bed6b95d072d78b39119d8ab1c75bde32bf
LTKSK <ltksk0714@gmail.com>
2025-10-23 08:35:33 +0900

    tessoku_cc

---
+++ tessoku_book_cc/main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE LambdaCase #-}
+import Data.Char (digitToInt, isSpace)
+import Data.List (sort, isSuffixOf, isPrefixOf, unfoldr)
+import Control.Monad (replicateM, msum, forM_)
+import Control.Monad.RWS (MonadState(put))
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Set as S
+import Data.Ix
+import Data.Array.IArray
+import Data.Array(Array)
+import Data.Array.Unboxed(UArray)
+
+ints :: IO [Int]
+ints = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+intwo :: IO (Int, Int)
+intwo =
+    ints >>= \case
+        [x, y] -> return (x,y)
+        _ -> error "引数足りない"
+
+getMatInt :: Int -> Int -> IO (UArray (Int, Int) Int)
+-- concatで多次元配列を1次元配列に
+getMatInt h w = listArray ((0,0), (h-1, w-1)) . concat <$> replicateM h ints
+
+binSearch :: (Int -> Bool) -> Int -> Int -> Int
+binSearch f ok ng
+  | abs (ok - ng) <= 1 = ok
+  | otherwise =
+    let mid = (ok + ng) `div` 2
+    in if f mid
+        then binSearch f mid ng -- 条件を満たすならmidをokに
+        else binSearch f ok mid -- 逆はngをmidに
+
+main :: IO ()
+main = do
+    [n] <- ints
+    let bits = reverse $ map digitToInt $ show n
+
+    print $ sum [2 ^ i | (i, v) <- zip [0..] bits, v == 1]
